4c8fc6d98f9cff27477b36b05b731db1c9bbffa8
SergeyPanchenko72 <143348555+SergeyPanchenko72@users.noreply.github.com>
2024-05-04 00:02:28 +0300

    init project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,27 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import "./App.module.css";
+import HomePage from "../../pages/HomePage/HomePage";
+import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
